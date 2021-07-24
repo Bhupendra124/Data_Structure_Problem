0d6d53d3c0179fc5e1badccd4f6d54db8bc45bf3
Bhupendra124 <wadekar.bhupendra6@gmail.com>
2021-07-25 01:32:22 +0530

    ADD:simple linkList using generic

---
+++ src/MainLinkedList.java
@@ -0,0 +1,16 @@
+import java.util.Iterator;
+
+public class MainLinkedList {
+    public static void main(String[] args) {
+        MyLinkedList myll= new MyLinkedList();
+        myll.add(56);
+        myll.add(30);
+        myll.add(70);
+
+myll.print();
+           
+       }
+    }
+
+
+
